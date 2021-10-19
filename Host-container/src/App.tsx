7da7e6073c7aa7b978e3aa3fe3ae759bb48994b3
Aleksandar Foltin <aleksandar.foltin@panonit.com>
2021-10-19 14:32:33 +0200

    registering all microfrontends from poc project, communication between them  working, maybe needs more polishing, will see

--- Host-container/src/App.tsx
+++ Host-container/src/App.tsx
@@ -14,6 +14,16 @@ declare global {
 
 function App() {
 
+  const localConstants = {
+    headerAppUrl: "http://localhost:4201",
+    navbarAppUrl: "http://localhost:4202",
+    capitalAppUrl: "http://localhost:4203",
+    transactionAppUrl: "http://localhost:4204",
+    chartAppUrl: "http://localhost:4205",
+
+    manifest: "manifest.json"
+  };
+
   useEffect(() => {
 
     console.log('host mounted');
@@ -22,19 +32,21 @@ function App() {
 
     const platformConfig: ApplicationConfig[] = [
       { symbolicName: 'host-app', manifestUrl: hostManifestPath },
-      { symbolicName: 'header-app', manifestUrl: `http://localhost:4201/manifest.json` },
-      { symbolicName: 'navbar-app', manifestUrl: `http://localhost:4202/manifest.json` },
-      { symbolicName: 'capital-app', manifestUrl: `http://localhost:4203/manifest.json` }
+      { symbolicName: 'header-app', manifestUrl: localConstants.headerAppUrl + '/' + localConstants.manifest },
+      { symbolicName: 'navbar-app', manifestUrl: localConstants.navbarAppUrl + '/' + localConstants.manifest },
+      { symbolicName: 'capital-app', manifestUrl: localConstants.capitalAppUrl + '/' + localConstants.manifest },
+      { symbolicName: 'chart-app', manifestUrl: localConstants.chartAppUrl + '/' + localConstants.manifest },
+      { symbolicName: 'transactions-app', manifestUrl: localConstants.transactionAppUrl + '/' + localConstants.manifest }
     ];
 
     async function init() {
       // Start the platform
       await MicrofrontendPlatform.startHost(platformConfig, { symbolicName: 'host-app' });
 
-      Beans.get(OutletRouter).navigate(`http://localhost:4201/header-app.html`, { outlet: 'HEADER' });
-      Beans.get(OutletRouter).navigate(`http://localhost:4202/navbar-app.html`, { outlet: 'NAVBAR' });
-
-      Beans.get(OutletRouter).navigate(`http://localhost:4203/index.html`);
+      Beans.get(OutletRouter).navigate(`${localConstants.headerAppUrl}/header-app.html`, { outlet: 'HEADER' });
+      Beans.get(OutletRouter).navigate(`${localConstants.navbarAppUrl}/navbar-app.html`, { outlet: 'NAVBAR' });
+      Beans.get(OutletRouter).navigate(`${localConstants.chartAppUrl}/index.html`, { outlet: 'MAIN-SCREEN-ASIDE' });
+      Beans.get(OutletRouter).navigate(`${localConstants.capitalAppUrl}/index.html`);
 
     }
 
