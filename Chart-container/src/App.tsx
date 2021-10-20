799ede0d6582f492b10d071cb304921f32fcd785
Aleksandar Foltin <aleksandar.foltin@panonit.com>
2021-10-20 09:40:46 +0200

    application code coppied to chart app folder, renamings done

---
+++ Chart-container/src/App.tsx
@@ -0,0 +1,31 @@
+import { useEffect } from 'react';
+import './App.css';
+
+import { MicrofrontendPlatform, OutletRouter } from '@scion/microfrontend-platform';
+import { Beans } from '@scion/toolkit/bean-manager';
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'sci-router-outlet': any;
+    }
+  }
+}
+
+function App() {
+
+  useEffect(() => {
+
+    console.log('chart mounted');
+
+  }, [])
+
+  return (
+    <div id="nca-chart-app-wrapper">
+      NCA CHART
+      <sci-router-outlet name="CHART"></sci-router-outlet>
+    </div>
+  );
+}
+
+export default App;
