b63d3afeb75cfd7d7df6329e7ba10a4eb387780c
Aleksandar Foltin <aleksandar.foltin@panonit.com>
2021-10-20 09:35:55 +0200

    navbar code coppied to transaction folder, renamings done

---
+++ Transaction-container/src/App.tsx
@@ -0,0 +1,31 @@
+import { useEffect } from 'react';
+import './App.css';
+
+import { MicrofrontendPlatform, OutletRouter } from '@scion/microfrontend-platform';
+import { Beans } from '@scion/toolkit/bean-manager';
+
+declare global {
+  namespace JSX {
+    interface IntrinsicElements {
+      'sci-router-outlet': any;
+    }
+  }
+}
+
+function App() {
+
+  useEffect(() => {
+
+    console.log('transaction mounted');
+
+  }, [])
+
+  return (
+    <div id="nca-transaction-app-wrapper">
+      NCA TRANSACTION
+      <sci-router-outlet name="TRANSACTION"></sci-router-outlet>
+    </div>
+  );
+}
+
+export default App;
